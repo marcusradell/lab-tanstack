edd2aaaedda5963d8dbbf33a9f223a16ce446f2e
Marcus Rådell <marcus@radell.net>
2024-10-22 09:16:19 +0200

    setup /status endpoint with express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,3 @@
 import { main } from "./main";
 
-console.log(main());
+main(8080);
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,15 @@
-export function main() {
+import express from "express";
+
+export function main(port: number) {
+  const app = express();
+
+  app.get("/status", (req, res) => {
+    res.json({ status: "READY" });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}.`);
+  });
+
   return "Hi!";
 }
